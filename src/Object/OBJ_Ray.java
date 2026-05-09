7a88a38bb664965297ccfd8edde2929c6493c47d
Perro Tostado <jhulianstewarpz@gmail.com>
2026-05-09 01:37:25 -0500

    Mejora en el rendimiento del juego, mejora en la impresión de los tiles

--- src/Object/OBJ_Ray.java
+++ src/Object/OBJ_Ray.java
@@ -2,12 +2,17 @@ package Object;
 
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import gta_java.GamePanel;
+import gta_java.UtilityTool;
 
 public class OBJ_Ray extends SuperObject {
-    public OBJ_Ray() {
+    public OBJ_Ray(GamePanel gp) {
         name = "Ray";
         try {
             image = ImageIO.read(getClass().getResourceAsStream("/res/objects/ray.png"));
+            UtilityTool uTool = new UtilityTool();
+            // Escalamos el objeto al tamaño del tile
+            image = uTool.scaleImage(image, gp.tileSize, gp.tileSize);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/gta_java/UI.java
+++ src/gta_java/UI.java
@@ -24,7 +24,7 @@ public class UI {
         arial_40 = new Font("Arial", Font.PLAIN, 40);
         arial_80B = new Font("Arial", Font.BOLD, 80);
         
-        OBJ_Ray ray = new OBJ_Ray(); 
+        OBJ_Ray ray = new OBJ_Ray(gp); 
         heartImage = ray.image;
     }
 
