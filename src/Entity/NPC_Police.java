cda6de88b2e9dac99db93e54aae9ee6e775df152
Ashley <albaashley57@gmail.com>
2026-05-17 02:22:16 -0500

    por alguna razon se habia borrado el NPC

--- src/Entity/NPC_Police.java
+++ src/Entity/NPC_Police.java
@@ -1,11 +1,8 @@
 package Entity;
 
 import gta_java.GamePanel;
-
 import java.util.List;
-import java.util.Random;
 import javax.imageio.ImageIO;
-
 import Grafo.Nodo;
 
 public class NPC_Police extends Entity {
@@ -19,10 +16,9 @@ public class NPC_Police extends Entity {
 
     public void getImage() {
         try {
-            // Asegúrate de tener estas imágenes en tu carpeta res/npc/
-            up = ImageIO.read(getClass().getResourceAsStream("/res/npc/policia_arr.png"));
-            down = ImageIO.read(getClass().getResourceAsStream("/res/npc/policia_aba.png"));
-            left = ImageIO.read(getClass().getResourceAsStream("/res/npc/policia_der.png"));
+            up    = ImageIO.read(getClass().getResourceAsStream("/res/npc/policia_arr.png"));
+            down  = ImageIO.read(getClass().getResourceAsStream("/res/npc/policia_aba.png"));
+            left  = ImageIO.read(getClass().getResourceAsStream("/res/npc/policia_der.png"));
             right = ImageIO.read(getClass().getResourceAsStream("/res/npc/policia_izq.png"));
         } catch (Exception e) {
             e.printStackTrace();
@@ -31,21 +27,16 @@ public class NPC_Police extends Entity {
 
     @Override
     public void setAction() {
-        // 1. Obtener coordenadas actuales y del jugador
         int policeCol = (worldX + gp.tileSize / 2) / gp.tileSize;
         int policeRow = (worldY + gp.tileSize / 2) / gp.tileSize;
         int playerCol = (gp.player.worldX + gp.player.solidArea.x) / gp.tileSize;
         int playerRow = (gp.player.worldY + gp.player.solidArea.y) / gp.tileSize;
 
-        // 2. Solo decidir nueva dirección si estamos alineados con el tile (evita atascos)
         if (worldX % gp.tileSize == 0 && worldY % gp.tileSize == 0) {
-            
             List<Nodo> ruta = gp.diGrafo.buscarCaminoAStar(policeRow, policeCol, playerRow, playerCol);
-
             if (ruta != null && ruta.size() > 1) {
-                Nodo siguiente = ruta.get(1); // El siguiente paso lógico
-
-                if (siguiente.getRow() < policeRow) direction = "up";
+                Nodo siguiente = ruta.get(1);
+                if      (siguiente.getRow() < policeRow) direction = "up";
                 else if (siguiente.getRow() > policeRow) direction = "down";
                 else if (siguiente.getCol() < policeCol) direction = "left";
                 else if (siguiente.getCol() > policeCol) direction = "right";
